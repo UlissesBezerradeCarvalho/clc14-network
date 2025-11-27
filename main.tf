45667b117d5b4d7fac75521fbf27985f33f9a77f
ulisses <ulissesht@hotmail.com>
2025-11-27 20:41:23 +0000

    vaii

--- main.tf
+++ main.tf
@@ -4,6 +4,17 @@ variable "vpc_name" {
   type = string
   default = "vpc-terraformc"
 }
+resource "aws_flow_log" "example" {
+  iam_role_arn    = "arn"
+  log_destination = "log"
+  traffic_type    = "ALL"
++ vpc_id          = aws_vpc.ok_vpc.id
+}
+
+resource "aws_vpc" "ok_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
 
 resource "aws_vpc" "minha_vpc" {
   cidr_block       = "10.0.0.0/16"
